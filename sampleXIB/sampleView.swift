57a2a1ec020bf026d2a37576f2b52a127f3f46b0
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2017-09-18 13:32:10 +0900

    最初の紐付け準備

---
+++ sampleXIB/sampleView.swift
@@ -0,0 +1,21 @@
+//
+//  sampleView.swift
+//  sampleXIB
+//
+//  Created by 水野祥子 on 2017/09/18.
+//  Copyright © 2017年 sachiko. All rights reserved.
+//
+
+import UIKit
+
+class sampleView: UIView {
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    
+
+}
