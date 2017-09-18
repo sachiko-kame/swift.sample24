47d3cb5c8113bb1806f31baf0d03474c2f64aca9
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2017-09-18 13:27:06 +0900

    Initial Commit

---
+++ sampleXIB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sampleXIB
+//
+//  Created by 水野祥子 on 2017/09/18.
+//  Copyright © 2017年 sachiko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
